5f3cb9794ce50fce992e2ddeb9434ff29f2586db
Mikael88 <mikaelnusantara@gmail.com>
2023-11-07 15:01:41 +0700

    change app.js to index.js

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,9 +1,7 @@
-// src/modules/user/service.js
-
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const config = require('../config/config');
-const UserRepository = require('../repositories/userRepository');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
+const UserRepository = require("../repositories/userRepository");
 
 class UserService {
   constructor() {
@@ -11,10 +9,12 @@ class UserService {
   }
 
   async signup(userData) {
-    const existingUser = await this.userRepository.findUserByEmail(userData.email);
+    const existingUser = await this.userRepository.findUserByEmail(
+      userData.email
+    );
 
     if (existingUser) {
-      throw new Error('Email is already in use');
+      throw new Error("Email is already in use");
     }
 
     // Add a 'points' field to the user data with an initial value of 1000.
@@ -33,16 +33,18 @@ class UserService {
     const user = await this.userRepository.findUserByEmail(email);
 
     if (!user) {
-      throw new Error('Invalid email or password');
+      throw new Error("Invalid email or password");
     }
 
     const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
-      throw new Error('Invalid email or password');
+      throw new Error("Invalid email or password");
     }
 
-    const token = jwt.sign({ userId: user._id }, config.secretKey, { expiresIn: '1h' });
+    const token = jwt.sign({ userId: user._id }, config.secretKey, {
+      expiresIn: "1h",
+    });
 
     return { user, token };
   }
