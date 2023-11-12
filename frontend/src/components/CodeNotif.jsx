79df4c57d6a089e7eeef4cdc377379b84ce06b1d
ansjch <you@example.com>
2023-11-13 00:40:27 +0700

    giftcode

--- frontend/src/components/CodeNotif.jsx
+++ frontend/src/components/CodeNotif.jsx
@@ -1,6 +1,25 @@
 import Container from './Container';
+import React from 'react';
+import Cookies from 'js-cookie';
+import { useEffect, useState } from 'react';
 
 export default function CodeNotif() {
+  const [GiftCode, setGiftCode] = useState([])
+  useEffect(()=> {
+    const fetchData = async () => {
+      const result = await fetch('http://localhost:3000/partner/generateGiftcode')
+    result.json().then(json => {
+      setGiftCode(json.GiftCode);
+    });
+    const token = response.data.token;
+    Cookies.set('auth_token', token);
+    console.log(token);
+    window.location.assign('/mitra/dashboard');
+  };
+    fetchData();
+  }, []);
+  console.log(GiftCode);
+      
   return (
     <section className="w-full bg-custom-primary">
       <Container className="py-5 lg:px-5">
@@ -9,8 +28,11 @@ export default function CodeNotif() {
 
             <h1 className="font-medium text-custom-tertiary text-lg mb-4">
               GIFTCODE / BARCODE
+              {GiftCode.map((code) => (
+                key={code}
+              ))}
             </h1>
-          
+         
           </div>
         </div>
       </Container>
--- frontend/src/components/CounterExc.jsx
+++ frontend/src/components/CounterExc.jsx
@@ -1,8 +1,21 @@
-import Container from './Container';
-import kopi from '../assets/images/kopi-reward.jpg';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import kopi from '../assets/images/kopi-reward.jpg'; 
+import Container from './Container';
 
-export default function CounterExc() {
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const decreaseCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
   return (
     <section className="w-full bg-custom-primary">
       <Container className="py-5 lg:px-5">
@@ -24,7 +37,7 @@ export default function CounterExc() {
                   </label>
                   <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent my-5">
                     <button
-                      data-action="decrement"
+                      onClick={decreaseCount}
                       className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none"
                     >
                       <span className="m-auto text-2xl font-thin">−</span>
@@ -33,10 +46,11 @@ export default function CounterExc() {
                       type="number"
                       className="focus:outline-none text-center w-full bg-gray-300 font-semibold text-md hover:text-black focus:text-black md:text-base cursor-default flex items-center text-gray-700 outline-none"
                       name="custom-input-number"
-                      value="0"
+                      value={count}
+                      readOnly
                     />
                     <button
-                      data-action="increment"
+                      onClick={increaseCount}
                       className="bg-gray-300 text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer"
                     >
                       <span className="m-auto text-2xl font-thin">+</span>
@@ -44,18 +58,19 @@ export default function CounterExc() {
                   </div>
                 </div>
                 <Link to="/user/berhasil">
-                <button
-                  className="w-full lg:w-1/4 btn btn-accent btn-outline rounded-full lg:px-20 text-custom-primary my-20">Tukarkan
+                  <button
+                    className="w-full lg:w-1/4 btn btn-accent btn-outline rounded-full lg:px-20 text-custom-primary my-20"
+                  >
+                    Tukarkan
                   </button>
-                  </Link>
+                </Link>
               </div>
             </div>
           </div>
         </div>
-
-
       </Container>
     </section>
-    
   );
-}
+};
+
+export default Counter;
