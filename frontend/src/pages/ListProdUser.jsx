04f198003863d613cb4d98b83c2ec4a96445a759
Annisa J. Chandra <72086975+einnaisei@users.noreply.github.com>
2023-11-14 21:16:19 +0700

    Update ListProdUser.jsx

--- frontend/src/pages/ListProdUser.jsx
+++ frontend/src/pages/ListProdUser.jsx
@@ -1,9 +1,10 @@
 
-import { Footer, WasteOpt, RewardOpt, KedaiOption } from '../components';
+import { Navbar, Footer, WasteOpt, RewardOpt, KedaiOption } from '../components';
 
 export default function ListProdUser() {
     return (
       <>
+        <Navbar />
         <WasteOpt />
         <KedaiOption />
         <RewardOpt />
