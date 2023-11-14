b2c6bf64a8a8e0d12ecc15845343700df1749bdb
Fandi Hasnur <fandi.hasnur10@gmail.com>
2023-11-15 00:41:17 +0800

    chore: resize images and heading (#23)

--- frontend/src/components/AboutHome.jsx
+++ frontend/src/components/AboutHome.jsx
@@ -9,12 +9,12 @@ export default function Hero() {
         <h2 className="text-2xl font-bold tracking-tighter text-center lg:text-4xl lg:mt-20 text-custom-tertiary">
           Tentang Kami
         </h2>
-        <div className="flex flex-col lg:flex-row">
-          <div className="self-end mt-6 lg:w-1/2 lg:my-20">
+        <div className="flex flex-col justify-center lg:flex-row">
+          <div className="self-end mt-6 lg:w-1/3 lg:my-20">
             <img src={w2tImage} />
           </div>
           <div className="self-center lg:w-1/4 lg:mx-10">
-            <h1 className="text-3xl font-bold tracking-tighter lg:text-6xl text-custom-tertiary mt-7">
+            <h1 className="text-3xl font-bold tracking-tighter lg:text-5xl text-custom-tertiary mt-7">
               Waste2Trade<span className="lg:block"> Indonesia</span>
             </h1>
             <p className="my-5 text-md lg:text-base lg:my-6 text-custom-tertiary">
