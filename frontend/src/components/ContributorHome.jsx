cbb304b7f5f131bd57fc7e5bd2bb74cf4f933df2
Anjas <anjasopo@gmail.com>
2023-11-16 17:12:50 +0700

    delete: remove code opacity-60 ContributorHome.jsx

--- frontend/src/components/ContributorHome.jsx
+++ frontend/src/components/ContributorHome.jsx
@@ -6,33 +6,33 @@ import coffe4 from '../assets/images/coffe4.png';
 
 export default function ContributorHome() {
   return (
-    <section className="w-full bg-custom-primary py-5 lg:py-10">
+    <section className="w-full py-5 bg-custom-primary lg:py-10">
       <Container>
-        <p className="text-base lg:text-lg lg:text-md text-center  text-custom-tertiary">
+        <p className="text-base text-center lg:text-lg lg:text-md text-custom-tertiary">
           Mitra
         </p>
-        <h2 className="text-2xl lg:text-4xl mb-4 lg:mb-8 font-bold tracking-tighter text-center text-custom-tertiary">
+        <h2 className="mb-4 text-2xl font-bold tracking-tighter text-center lg:text-4xl lg:mb-8 text-custom-tertiary">
           Agen Perubahan
         </h2>
         <div className="flex flex-wrap items-center justify-center">
           <img
             src={coffe1}
-            className="max-w-[120px] lg:max-w-[180px] grayscale opacity-60 transition hover:grayscale-0 opacity-100"
+            className="max-w-[120px] lg:max-w-[180px] grayscale transition hover:grayscale-0 opacity-100"
             alt="Coffe 1"
           />
           <img
             src={coffe2}
-            className="max-w-[120px] lg:max-w-[180px] grayscale opacity- transition hover:grayscale-0 opacity-100"
+            className="max-w-[120px] lg:max-w-[180px] grayscale transition hover:grayscale-0 opacity-100"
             alt="Coffe 2"
           />
           <img
             src={coffe3}
-            className="max-w-[120px] lg:max-w-[180px] grayscale opacity-60 transition hover:grayscale-0 opacity-100"
+            className="max-w-[120px] lg:max-w-[180px] grayscale transition hover:grayscale-0 opacity-100"
             alt="Coffe 3"
           />
           <img
             src={coffe4}
-            className="max-w-[120px] lg:max-w-[180px] grayscale opacity-60 transition hover:grayscale-0 opacity-100"
+            className="max-w-[120px] lg:max-w-[180px] grayscale transition hover:grayscale-0 opacity-100"
             alt="Coffe 4"
           />
         </div>
