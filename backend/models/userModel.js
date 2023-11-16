dac6c6b0f7fc2d8e6312fb03ad4b866d034b438d
Apple <apple@MacBook-Pro.local>
2023-11-16 23:14:36 +0700

    perbaikan userBE

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String,
-  points: { type: Number, default: 1000 },
+  points: { type: Number, default: 10 },
 });
 
 module.exports = mongoose.model("User", userSchema);
